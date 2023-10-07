3adb5de94c5c79acbbc7630e85edf3a783c58b51
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-10-06 23:04:08 -0300

    alterações

--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { Container, Content, Icon } from "./styles";
 
 import { groupCreate } from "@storage/group/groupCreate";
@@ -9,6 +10,7 @@ import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Highlight } from "../../components/HighLight";
 import { Input } from "../../components/Input";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
@@ -17,10 +19,19 @@ export function NewGroup() {
 
   async function handleAddTeams() {
     try {
+      if (group.trim().length === 0) {
+        Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Erro", "Ocorreu um erro ao criar o grupo");
+        console.log(error);
+      }
     }
   }
 
